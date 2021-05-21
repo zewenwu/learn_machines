f31e5af047bb992fd0c67e7312bb89dc747ba8d5
Ze Wen Wu <17549624+zewenwu@users.noreply.github.com>
2021-05-21 19:26:04 +0200

    worked on bouffon_needle

---
+++ bouffon_needle/bouffon_needle.py
@@ -0,0 +1,25 @@
+
+import random, math
+
+# Set up Bouffon's experiment
+N = 100000
+total_intersected = 0
+
+# Throw the N needles
+for i in range(0, N):
+    theta = random.uniform(0, math.pi/2)
+    x = random.uniform(0, 1)
+    needle_intersected = (x < math.sin(theta)/2)
+    if (needle_intersected):
+        total_intersected += 1
+
+# Getting the results
+pi_estimate = N/total_intersected
+
+print("\nBouffon's estimation of pi: ", pi_estimate)
+print("Value of pi: ", math.pi, "\n")
+
+print("Absolute error: ", abs(pi_estimate - math.pi))
+print("Relative absolute error: ", abs(pi_estimate - math.pi)/math.pi)
+
+
